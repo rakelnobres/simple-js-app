46ca13083c9864174c9d630725a6dba87248e35f
rakelnobres <rakelnobres@gmail.com>
2022-04-22 09:24:29 -0400

    created list of pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,62 @@
-alert('Hello world');
+// list of pokemon data
+let pokemonList = [
+  {
+    name: 'Ivysaur',
+    height: 1,
+    type: ['grass', 'poison'],
+  }
 
-let favoriteFood = 'spaghetti';
-document.write(favoriteFood);
+  {
+    name: 'Beedrill',
+    height: 1,
+    type: ['bug', 'poison'],
+  }
+
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: 'electric',
+  }
+
+  {
+    name: 'Raichu',
+    height: 0.8,
+    type: 'electric',
+  }
+
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+  }
+
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: 'water'
+  }
+
+  {
+    name: 'Kakuna',
+    height: 0.6,
+    type: ['bug', 'poison']
+  }
+
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    type: ['flying', 'normal']
+  }
+
+  {
+    name: 'Arbok',
+    height: 3.5,
+    type: 'poison'
+  }
+
+  {
+    name: 'Persian',
+    height: 1,
+    type: 'normal'
+  }
+];
